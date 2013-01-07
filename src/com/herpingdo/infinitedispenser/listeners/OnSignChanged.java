73b6b562adc63b317b953bb2762e49497878e82c
Ryan K <derp@derpy.fr>
2013-01-07 11:37:15 -0500

    Now it really works!

--- src/com/herpingdo/infinitedispenser/listeners/OnSignChanged.java
+++ src/com/herpingdo/infinitedispenser/listeners/OnSignChanged.java
@@ -5,8 +5,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
 import com.herpingdo.infinitedispenser.Utils;
 
@@ -22,11 +20,13 @@ public class OnSignChanged implements Listener {
 			if (!p.hasPermission("infinitedispenser.create") && !p.isOp())
 			{
 				p.sendMessage(ChatColor.RED+"[InfiniteDispenser] You may not make infinite dispensers!");
-				p.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 2, 5));
+				p.setFoodLevel(0);
+				/* 0.5 heart */
+				p.setHealth(1);
 				event.setCancelled(true);
 				return;
 			}
-			/* TODO: Check if it's actually next to a dispenser. */
+			event.setLine(0, "[Infinite]");
 			event.getPlayer().sendMessage(ChatColor.GREEN+"[InfiniteDispenser]"+ChatColor.YELLOW+" You just created an infinite dispenser!");
 		}
 	}
